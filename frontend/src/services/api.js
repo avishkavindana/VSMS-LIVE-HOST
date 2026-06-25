490727121a559c928bdafa57d123f88dbc14505f
avishkavindana <avishkavindana22@gmail.com>
2026-06-25 18:12:08 +0530

    Update baseURL for API service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://vsms-live-host-backend.onrender.com/api"
+  baseURL: "https://vsms-live-host-backend.onrender.com"
 });
